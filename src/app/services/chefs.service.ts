1e5b5af099458ec4de51e21382e6a4c2c1958d97
Oz Hogeg <ozh@moveo.co.il>
2022-01-17 09:50:47 +0200

    added restaurant filed in dishes and fixed chef of the week when it's update

--- src/app/services/chefs.service.ts
+++ src/app/services/chefs.service.ts
@@ -4,95 +4,116 @@ import { API_URL } from '../assets/constants/constants';
 import { IChef, IServerResponse } from '../assets/models';
 import { AuthService } from './auth.service';
 
-
 @Injectable({ providedIn: 'root' })
 export class ChefsService {
-    @Output() serverResponseEvent = new EventEmitter<IServerResponse>();
-    @Output() chefsUpdateEvent = new EventEmitter<IChef[]>();
+  @Output() serverResponseEvent = new EventEmitter<IServerResponse>();
+  @Output() chefsUpdateEvent = new EventEmitter<IChef[]>();
 
-    readonly API = API_URL.chefs;
+  readonly API = API_URL.chefs;
+  chefs: IChef[] = [];
 
-    chefs: IChef[] = [];
+  constructor(private http: HttpClient, private authService: AuthService) {
+    console.log('chefs constructor');
+    this.fetchChefs();
+  }
 
-    constructor(private http: HttpClient, private authService: AuthService) {
-        console.log("chefs constructor");
-        this.fetchChefs();
+  fetchChefs() {
+    this.http.get(this.API, this.authService.authorizationHeader).subscribe({
+      next: this.fetchChefsHandler.bind(this),
+      error: this.errorHandler.bind(this),
+    });
+  }
+
+  fetchChefsHandler(chefs: IChef[]) {
+    console.log('fetchChefsHandler', chefs);
+    this.chefs = chefs;
+    this.chefsUpdateEvent.emit(this.chefs);
+  }
+
+  updateChef(chef: IChef) {
+    this.http
+      .put(
+        `${this.API}/${chef._id}`,
+        chef,
+        this.authService.authorizationHeader
+      )
+      .subscribe({
+        next: this.updateChefHandler.bind(this),
+        error: this.errorHandler.bind(this),
+      });
+  }
+
+  updateChefHandler(responseChef: IChef) {
+    console.log('response put chef', responseChef);
+    const updatedChefIndex = this.chefs.findIndex(
+      (chef) => chef._id === responseChef._id
+    );
+
+    if (responseChef.chefOfTheWeek) this.removeChefOfTheWeek();
+
+    this.chefs[updatedChefIndex] = responseChef;
+    this.chefsUpdateEvent.emit(this.chefs);
+    this.serverResponseEvent.emit({ valid: true, httpMethodRequest: 'PUT' });
+  }
+
+  removeChefOfTheWeek() {
+    this.chefs.forEach((chef) => {
+      if (chef.chefOfTheWeek) {
+        chef.chefOfTheWeek = false;
+        return;
+      }
+    });
+  }
+
+  postChef(chef: IChef) {
+    this.http
+      .post(this.API, chef, this.authService.authorizationHeader)
+      .subscribe({
+        next: this.postChefHandler.bind(this),
+        error: this.errorHandler.bind(this),
+      });
+  }
+
+  postChefHandler(responseChef: IChef) {
+    console.log('response post chef', responseChef);
+    this.chefs.push(responseChef);
+    this.chefsUpdateEvent.emit(this.chefs);
+    this.serverResponseEvent.emit({ valid: true, httpMethodRequest: 'POST' });
+  }
+
+  deleteChef(chef: IChef) {
+    this.http
+      .delete(`${this.API}/${chef._id}`, this.authService.authorizationHeader)
+      .subscribe({
+        next: this.deleteChefHandler.bind(this),
+        error: this.errorHandler.bind(this),
+      });
+  }
+
+  deleteChefHandler(response: { message: string; id: string }) {
+    console.log('response delete chef', response);
+    const deletedChefIndex = this.chefs.findIndex(
+      (chef) => chef._id === response.id
+    );
+
+    this.chefs.splice(deletedChefIndex, 1);
+    this.chefsUpdateEvent.emit(this.chefs);
+    this.serverResponseEvent.emit({ valid: true, httpMethodRequest: 'DELETE' });
+  }
+
+  errorHandler(err) {
+    console.log('errorHandler', err);
+    const serverResponse: IServerResponse = {
+      valid: false,
+      message: err.error,
+    };
+    switch (err.status) {
+      case 403:
+        console.log('chefs service error handler access forbidden');
+        this.authService.onAccessForbidden();
+        break;
+      default:
+        this.serverResponseEvent.emit(serverResponse);
     }
-
-    fetchChefs() {
-        this.http.get(this.API, this.authService.authorizationHeader).subscribe({
-            next: this.fetchChefsHandler.bind(this),
-            error: this.errorHandler.bind(this)
-        })
-    }
-
-    fetchChefsHandler(chefs: IChef[]) {
-        console.log("fetchChefsHandler", chefs);
-        this.chefs = chefs;
-        this.chefsUpdateEvent.emit(this.chefs);
-    }
-
-    updateChef(chef: IChef) {
-        this.http.put(`${this.API}/${chef._id}`, chef, this.authService.authorizationHeader).subscribe({
-            next: this.updateChefHandler.bind(this),
-            error: this.errorHandler.bind(this)
-        })
-    }
-
-    updateChefHandler(responseChef: IChef) {
-        console.log("response put chef", responseChef);
-        const updatedChefIndex = this.chefs.findIndex(chef => chef._id === responseChef._id);
-
-        this.chefs[updatedChefIndex] = responseChef;
-        this.chefsUpdateEvent.emit(this.chefs);
-        this.serverResponseEvent.emit({ valid: true, httpMethodRequest: "PUT" });
-    }
-
-    postChef(chef: IChef) {
-        this.http.post(this.API, chef, this.authService.authorizationHeader).subscribe({
-            next: this.postChefHandler.bind(this),
-            error: this.errorHandler.bind(this)
-        });
-
-    }
-
-    postChefHandler(responseChef: IChef) {
-        console.log("response post chef", responseChef);
-        this.chefs.push(responseChef);
-        this.chefsUpdateEvent.emit(this.chefs);
-        this.serverResponseEvent.emit({ valid: true, httpMethodRequest: "POST" });
-    }
-
-    deleteChef(chef: IChef) {
-        this.http.delete(`${this.API}/${chef._id}`, this.authService.authorizationHeader).subscribe({
-            next: this.deleteChefHandler.bind(this),
-            error: this.errorHandler.bind(this)
-        })
-    }
-
-    deleteChefHandler(response: { message: string, id: string }) {
-        console.log("response delete chef", response);
-        const deletedChefIndex = this.chefs.findIndex(chef => chef._id === response.id);
-        
-        this.chefs.splice(deletedChefIndex, 1);
-        this.chefsUpdateEvent.emit(this.chefs);
-        this.serverResponseEvent.emit({ valid: true, httpMethodRequest: "DELETE" });
-    }
-
-    errorHandler(err) {
-        console.log("errorHandler", err);
-        const serverResponse: IServerResponse = {
-            valid: false,
-            message: err.error,
-        }
-        switch (err.status) {
-            case 403:
-                console.log("chefs service error handler access forbidden")
-                this.authService.onAccessForbidden();
-                break;
-            default:
-                this.serverResponseEvent.emit(serverResponse);
-        }
-    }
+  }
 }
-
